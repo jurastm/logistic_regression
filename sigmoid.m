f6b9bb782807c8a68c2e3f81198f8fef6642aeb4
jurastm <chinamobi.ru@gmail.com>
2016-06-24 16:54:35 +0300

    Update sigmoid.m

--- sigmoid.m
+++ sigmoid.m
@@ -1,7 +1,9 @@
 function g = sigmoid(z)
 
 g = zeros(size(z));
-% This function computes sigmoid of z (usually X*theta)
+% This function computes sigmoid of z. Usually X*theta, whereas matrix X is entire training set and vector theta is a set of
+% parameters. So, whenever X is m-by-n matrix. m is equal to number of training examples and n is equal to number of features.
+% Moreover, lenght of vector theta should always be equal to n (if not counting bias unit)
 g = 1 ./ (1 + exp(-z));
 
 end
