1d1eae8f82c529d932e5bfeb6389c9de2b9892e7
jurastm <chinamobi.ru@gmail.com>
2016-06-24 21:48:05 +0300

    Create mapFeature.m
    return polynomial features dataset

---
+++ mapFeature.m
@@ -0,0 +1,15 @@
+function out = mapFeature(X1, X2)
+
+% thi function creates  new feature array 
+
+degree = 6;
+out = ones(size(X1(:, 1)));
+
+for i = 1: degree
+    for j = 0:i
+    out(:, end+1) = (X1 .^ (i-j)).*(X2 .^ j)
+  end
+end
+
+
+end
