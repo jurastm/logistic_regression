d3815a1956f1af698c5f6dd95d18113028708a5b
jurastm <chinamobi.ru@gmail.com>
2016-07-07 20:19:54 +0300

    Update costFunction.m

--- costFunction.m
+++ costFunction.m
@@ -12,6 +12,6 @@ thetaT(1) = 0;
 reg = (lambda/(2*m))*sum(thetaT .^ 2);
 
 J = (1/m)*sum(temp1 - temp2) + reg;
-grad = (1/m)*(X'*(sigmoid(X*theta) - y) + thetaT*(lambda/m);
+grad = (1/m)*(X'*(sigmoid(X*theta) - y)) + thetaT*(lambda/m);
 
 end
